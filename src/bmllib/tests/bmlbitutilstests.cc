68bf2995f20534e2413658e13373b9e0e3f6562e
Alder9 <thomas.alder@colorado.edu>
2021-01-07 15:00:57 -0700

    Github Actions CI Attempt #1 on Windows 2019

--- src/bmllib/tests/bmlbitutilstests.cc
+++ src/bmllib/tests/bmlbitutilstests.cc
@@ -1,2 +1,14 @@
 #include "gtest/gtest.h"
 #include "../core/bmlbitutils.h"
+
+namespace bmllib {
+
+	TEST(LeftShiftTest, HandlesSingleByte) {
+		EXPECT_EQ(0, 0); // test to see if CTest working
+	}
+
+	TEST(LeftShiftTest, HandlesSingleByte2) {
+		EXPECT_EQ(0, 0); // test to see if CTest working
+	}
+
+}
