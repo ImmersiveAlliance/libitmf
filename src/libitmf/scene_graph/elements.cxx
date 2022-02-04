c3ea5ca32302fb2c05ce4e510a2fd2c190ce9c24
Austin Pahl <austinpahl@gmail.com>
2022-02-04 16:48:58 -0700

    Convert spaces to tabs in elements.cxx

--- src/libitmf/scene_graph/elements.cxx
+++ src/libitmf/scene_graph/elements.cxx
@@ -2,20 +2,20 @@
 
 namespace itmf {
 namespace scene {
-    const std::string Root::LATEST_VERSION = "1";
+	const std::string Root::LATEST_VERSION = "1";
 
-    boost::optional<AttributePtr> Item::getAttribute(AttributeId id) {
-        if (this->attributes.count(id) > 0)
-            return this->attributes[id];
-        else
-            return boost::none;
-    }
+	boost::optional<AttributePtr> Item::getAttribute(AttributeId id) {
+		if (this->attributes.count(id) > 0)
+			return this->attributes[id];
+		else
+			return boost::none;
+	}
 
-    boost::optional<Pin> Node::getPin(PinId id) {
-        if (this->pins.count(id) > 0)
-            return this->pins[id];
-        else
-            return boost::none;
-    }
+	boost::optional<Pin> Node::getPin(PinId id) {
+		if (this->pins.count(id) > 0)
+			return this->pins[id];
+		else
+			return boost::none;
+	}
 }
 }
